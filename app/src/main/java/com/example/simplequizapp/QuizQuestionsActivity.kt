a33fd66eb6ece379a15cfa4d91f92eee46686454
Dmitrii Chernyshov <Dmitrii_Chernyshov@jabil.com>
2023-06-15 14:57:50 +0200

    added question model and bootstrap data

---
+++ app/src/main/java/com/example/simplequizapp/QuizQuestionsActivity.kt
@@ -0,0 +1,15 @@
+package com.example.simplequizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class QuizQuestionsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz_questions)
+
+        val questionsList = Constants.getQuestions()
+        Log.i("QuizQuestionsActivity", "QuestionsList size is: ${questionsList.size}")
+    }
+}
